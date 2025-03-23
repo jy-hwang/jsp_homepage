62e6f94e0adef83de4d97e604280270df73e3949
jyhwang <stratojy@gmail.com>
2025-03-23 20:54:38 +0900

    fix. filter 작업 추가
    1. filter 에서 아무런 작업도 하고 있지 않아서 UTF-8 설정하는 부분을 추가하였음.

--- src/main/java/filter/CharacterEncodingFilter.java
+++ src/main/java/filter/CharacterEncodingFilter.java
@@ -12,9 +12,10 @@ public class CharacterEncodingFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    System.out.println("before filter");
+
+    request.setCharacterEncoding("UTF-8");
+    
     chain.doFilter(request, response);
-    System.out.println("after filter");
 
   }
 
