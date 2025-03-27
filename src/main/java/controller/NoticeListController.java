4c7f5f5516a2c7019caa79bebc4a52d7b1890d41
jyhwang <stratojy@gmail.com>
2025-03-27 22:45:45 +0900

    feat. Controller <-> Service 분리
    1. 컨트롤러의 소스를 서비스로 옮기고
    2. 컨트롤러에서는 서비스의 메서드를 호출

--- src/main/java/controller/NoticeListController.java
+++ src/main/java/controller/NoticeListController.java
@@ -1,12 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import entity.Notice;
+import service.NoticeService;
 
 @WebServlet("/notice/list")
 public class NoticeListController extends HttpServlet {
@@ -24,62 +19,16 @@ public class NoticeListController extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    List<Notice> list = new ArrayList<Notice>();
+    NoticeService service = new NoticeService();
 
-    String dbUrl = System.getProperty("db.url");
-    String dbUsername = System.getProperty("db.username");
-    String dbPassword = System.getProperty("db.password");
-    Connection conn = null;
-    Statement stmt = null;
-    ResultSet rSet = null;
+    List<Notice> list = service.getNoticeList();
 
-    try {
 
-      Class.forName("org.mariadb.jdbc.Driver");
 
-      conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    request.setAttribute("list", list);
+    // forward : 앞 뒤를 연결하는 '같은 요청'.
+    request.getRequestDispatcher("/WEB-INF/views/notice/list.jsp").forward(request, response);
 
-      String query =
-          " SELECT no, writer_id AS writerId, title, content, hit, files, created_date AS createdDate, updated_date AS updatedDate FROM notice; ";
 
-      stmt = conn.createStatement();
-
-      rSet = stmt.executeQuery(query);
-
-      while (rSet.next()) {
-
-        int noticeNo = rSet.getInt("no");
-        String title = rSet.getString("title");
-        String createdDate = rSet.getString("createdDate");
-        String writerId = rSet.getString("writerId");
-        String files = rSet.getString("files") == null ? "" : rSet.getString("files");
-        String content = rSet.getString("content");
-        int hit = rSet.getInt("hit");
-
-        Notice notice = new Notice(noticeNo, title, createdDate, writerId, files, content, hit);
-        list.add(notice);
-      }
-      request.setAttribute("list", list);
-      // forward : 앞 뒤를 연결하는 '같은 요청'.
-      request.getRequestDispatcher("/WEB-INF/views/notice/list.jsp").forward(request, response);
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        if (rSet != null) {
-          rSet.close();
-        }
-        if (stmt != null) {
-          stmt.close();
-        }
-        if (conn != null) {
-          conn.close();
-        }
-      } catch (SQLException e2) {
-        e2.printStackTrace();
-      }
-    }
   }
 }
--- src/main/java/service/NoticeService.java
+++ src/main/java/service/NoticeService.java
@@ -1,9 +1,17 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import entity.Notice;
 
 public class NoticeService {
+
+  public static final int PAGE_SIZE = 5;
 
   public List<Notice> getNoticeList() {
 
@@ -16,10 +24,80 @@ public class NoticeService {
   }
 
   public List<Notice> getNoticeList(String field, String keyword, int page) {
-    String query =
-        " SELECT no, writer_id AS writerId, title, content, hit, files, created_date AS createdDate, updated_date AS updatedDate FROM notice LIMIT ? OFFSET ? ";
+    List<Notice> list = new ArrayList<Notice>();
 
-    return null;
+    // field <<- title, content, writer_id
+
+    String query =
+        " SELECT no, writer_id AS writerId, title, content, hit, files, created_date AS createdDate, updated_date AS updatedDate FROM notice"
+            + " WHERE "+ field +" LIKE ? LIMIT ? OFFSET ? ";
+
+    /*
+     * 1, 6, 11, 16, 21, ... => an = 1 + (page - 1) * 5
+     * 5, 10, 15, 20, ... => page * 5
+     * pageSize 5
+     * page 4
+     * offset = (page - 1) * pageSize = (4 - 1) * 5 = 15
+     *
+     */
+
+    int offset = (page - 1) * PAGE_SIZE;
+
+    String dbUrl = System.getProperty("db.url");
+    String dbUsername = System.getProperty("db.username");
+    String dbPassword = System.getProperty("db.password");
+    Connection conn = null;
+    PreparedStatement pStmt = null;
+    ResultSet rSet = null;
+
+    try {
+
+      Class.forName("org.mariadb.jdbc.Driver");
+
+      conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+      pStmt = conn.prepareStatement(query);
+      pStmt.setString(1, "%" + keyword + "%");
+      pStmt.setInt(2, PAGE_SIZE);
+      pStmt.setInt(3, offset);
+      
+      rSet = pStmt.executeQuery();
+
+      while (rSet.next()) {
+
+        int noticeNo = rSet.getInt("no");
+        String title = rSet.getString("title");
+        String createdDate = rSet.getString("createdDate");
+        String writerId = rSet.getString("writerId");
+        String files = rSet.getString("files") == null ? "" : rSet.getString("files");
+        String content = rSet.getString("content");
+        int hit = rSet.getInt("hit");
+
+        Notice notice = new Notice(noticeNo, title, createdDate, writerId, files, content, hit);
+        list.add(notice);
+      }
+      
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (rSet != null) {
+          rSet.close();
+        }
+        if (pStmt != null) {
+          pStmt.close();
+        }
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException e2) {
+        e2.printStackTrace();
+      }
+    }
+
+    return list;
   }
 
   public int getNoticeCount() {
@@ -29,28 +107,28 @@ public class NoticeService {
 
   public int getNoticeCount(String field, String keyword) {
     String query = " SELECT count(*) as totalCount FROM notice ";
-    
+
     return 0;
   }
 
   public Notice getNotice(int id) {
     String query =
-        " SELECT no, writer_id AS writerId, title, content, hit, files, created_date AS createdDate, updated_date AS updatedDate FROM notice WHERE no = ?";
-    
+        " SELECT no, writer_id AS writerId, title, content, hit, files, created_date AS createdDate, updated_date AS updatedDate FROM notice WHERE no = ? ";
+
     return null;
   }
 
   public Notice getNextNotice(int id) {
     String query =
-        " SELECT title FROM notice WHERE created_date > ( SELECT created_date FROM notice WHERE NO = 1) LIMIT 1 ";
-    
+        " SELECT title FROM notice WHERE created_date > ( SELECT created_date FROM notice WHERE NO = ?) LIMIT 1 ";
+
     return null;
   }
 
   public Notice getPrevNotice(int id) {
     String query =
-        " SELECT title FROM notice WHERE created_date < ( SELECT created_date FROM notice WHERE NO = 2) ORDER BY created_date DESC LIMIT 1; ";
-    
+        " SELECT title FROM notice WHERE created_date < ( SELECT created_date FROM notice WHERE NO = ?) ORDER BY created_date DESC LIMIT 1 ";
+
     return null;
   }
 
